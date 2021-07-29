8dbf44671257ec94d6acdae7fa1b654f4112bda1
Ivan Delic <ivan.delic@gmail.com>
2021-07-29 12:26:30 +0200

    creating more generic function to cover querystring and multiplexing services

--- diego-proxy-fn/src/main/java/com/diego/fn/DiegoAuthProxy.java
+++ diego-proxy-fn/src/main/java/com/diego/fn/DiegoAuthProxy.java
@@ -8,22 +8,61 @@ import java.util.Base64;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fnproject.fn.api.httpgateway.HTTPGatewayContext;
 
 public class DiegoAuthProxy {
 
-    public String handleRequest(String input) {
-        String tokenUrl = System.getenv("TOKEN_URL");
-        String serviceUrl = System.getenv("SERVICE_URL");
-        String username = System.getenv("USERNAME");
-        String password = System.getenv("PASSWORD");
-        String clientCredentials = new String(Base64.getEncoder().encode(new String(System.getenv("CLIENT_ID") + ":" + System.getenv("CLIENT_SECRET")).getBytes()));
-        String scope = System.getenv("SCOPE");
-        
+    private static final String DEFAULT_SERVICE_METHOD = "POST";
+
+    private static final String PAYLOAD_JSON = "payload";
+    private static final String PASSWORD_JSON = "password";
+    private static final String USERNAME_JSON = "username";
+    private static final String SERVICE_METHOD_JSON = "service-method";
+    private static final String SERVICE_URL_JSON = "service-url";
+    private static final String TOKEN_URL_JSON = "token-url";
+
+    private static final String SCOPE = "SCOPE";
+    private static final String CLIENT_SECRET = "CLIENT_SECRET";
+    private static final String CLIENT_ID = "CLIENT_ID";
+    private static final String PASSWORD = "PASSWORD";
+    private static final String USERNAME = "USERNAME";
+    private static final String SERVICE_URL = "SERVICE_URL";
+    private static final String TOKEN_URL = "TOKEN_URL";
+
+    public String handleRequest(HTTPGatewayContext hctx, String rawInput) {
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode input = mapper.createObjectNode();
+        try {
+            input = new ObjectMapper().readTree(rawInput);
+        }
+        catch (Exception e) {
+            e.printStackTrace(); // not valid input json
+        }
+
+        String tokenUrl = !input.isEmpty() && input.has(TOKEN_URL_JSON) ? input.get(TOKEN_URL_JSON).asText() : System.getenv(TOKEN_URL);
+        String serviceUrl = !input.isEmpty() && input.has(SERVICE_URL_JSON) ? input.get(SERVICE_URL_JSON).asText() : System.getenv(SERVICE_URL);
+        String serviceMethod = !input.isEmpty() && input.has(SERVICE_METHOD_JSON) ? input.get(SERVICE_METHOD_JSON).asText() : DEFAULT_SERVICE_METHOD;
+        String username = !input.isEmpty() && input.has(USERNAME_JSON) ? input.get(USERNAME_JSON).asText() : System.getenv(USERNAME);
+        String password = !input.isEmpty() && input.has(PASSWORD_JSON) ? input.get(PASSWORD_JSON).asText() : System.getenv(PASSWORD);
+        String clientCredentials = new String(Base64.getEncoder().encode(new String(System.getenv(CLIENT_ID) + ":" + System.getenv(CLIENT_SECRET)).getBytes()));
+        String scope = System.getenv(SCOPE);
+
+        String querystring = extractQuerystring(hctx.getRequestURL());
+        String payload = !input.isEmpty() && input.has(PAYLOAD_JSON) ? input.get(PAYLOAD_JSON).asText() : null;
+
         String tokenResponse = retriveAccessToken(tokenUrl, username, password, scope, clientCredentials);
         String accessToken = extractAccessToken(tokenResponse);
-        String serviceResponse = retriveService(serviceUrl, accessToken);
+        String serviceResponse = retriveService(serviceUrl, accessToken, serviceMethod, querystring, payload);
 
-        return "{\"access_token\": \"" + accessToken + "\", \"external_service_payload\": \"" + serviceResponse + "\"}";
+        return "{\"access_token\": \"" + accessToken + "\", \"external_service_payload\": \"" + serviceResponse + "\", \"input\": \"" + input.toString() + "\", \"querystring\": \"" + querystring + "\"}";
+    }
+
+    private String extractQuerystring(String requestUrl) {
+        if (requestUrl == null || requestUrl.isEmpty() || !requestUrl.contains("?")) {
+            return "";
+        }
+        return requestUrl.substring(requestUrl.indexOf("?"));
     }
 
     private String retriveAccessToken(String url, String username,String password, String scope, String clientCredentials) {
@@ -58,15 +97,19 @@ public class DiegoAuthProxy {
         return response;
     }
 
-    private String retriveService(String url, String token) {
+    private String retriveService(String url, String token, String serviceMethod, String querystring, String payload) {
         String response = "";
 
         try {
             HttpURLConnection httpConn = (HttpURLConnection) new URL(url).openConnection();
             httpConn.setUseCaches(false);
             httpConn.setDoOutput(true);
-            httpConn.setRequestMethod("GET");
+            httpConn.setRequestMethod(serviceMethod);
+            httpConn.setRequestProperty("Content-Type", "application/json");
             httpConn.setRequestProperty("Authorization", "Bearer " + token);
+            if (payload != null) {
+                httpConn.getOutputStream().write(payload.getBytes(StandardCharsets.UTF_8));
+            }
             if (HttpURLConnection.HTTP_OK == httpConn.getResponseCode()) {
                 ByteArrayOutputStream result = new ByteArrayOutputStream();
                 byte[] buffer = new byte[1024];
